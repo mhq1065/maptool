4bae0491a153b949026a595b41b4ff4452767e69
mhq1065 <mhq1065@qq.com>
2020-07-06 22:27:56 +0800

    init echarts and antd

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <a-button type="primary">Button></a-button>
+    <div id="main" style="width: 600px;height:400px;"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    data() {
+      return {
+        myChart: {},
+        option: {
+          title: { text: "在Vue中使用echarts" },
+          tooltip: {},
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: [5, 20, 36, 10, 10, 20],
+            },
+          ],
+        },
+      };
+    },
+    mounted() {
+      this.myChart = this.$echarts.init(document.getElementById("main"));
+      this.drawChart();
+    },
+    methods: {
+      drawChart() {
+        console.log('draw charts')
+        // 使用刚指定的配置项和数据显示图表。
+        this.myChart.setOption(this.option);
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
+import echarts from 'echarts';
+import axios from 'axios';
 
+Vue.component(Button.name, Button);
+Vue.prototype.$axios = axios;
+Vue.prototype.$echarts = echarts;
 Vue.config.productionTip = false
 
 new Vue({
